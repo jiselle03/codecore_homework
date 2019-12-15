936b0e3b7b79d973c1365f91f8365d04a59b0fdd
Jiselle Liu <jiselle03@gmail.com>
2019-12-15 10:35:17 -0800

    Create Super Team Picker app, pending edit/delete functions

---
+++ super_team_picker/db/queries.js
@@ -0,0 +1,30 @@
+const knex = require("./client");
+
+module.exports = {
+  // get all cohorts
+  getAll() {
+    return knex("cohorts").select("*");
+  },
+  // get one cohort
+  getOne(id) {
+    return knex("cohorts")
+      .where("id", id)
+      .first();
+  },
+  // create new cohort
+  add(cohort) {
+    return knex("cohorts").insert(cohort, "*");
+  },
+  // edit cohort
+  edit(id, cohort) {
+    return knex("cohorts")
+      .where("id", id)
+      .update(cohort, "*");
+  },
+  // delete cohort
+  delete(id) {
+    return knex("cohorts")
+      .where("id", id)
+      .del();
+  }
+};
---
+++ super_team_picker/db/seeds/01_cohorts.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('cohorts').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('cohorts').insert([
+        {name: 'Gryffindor', members: 'Hermione Granger, Harry Potter, Ronald Weasley, Ginny Weasley, Neville Longbottom, Seamus Finnigan, Dean Thomas, Fred Weasley, George Weasley', logoUrl: 'https://images-na.ssl-images-amazon.com/images/I/915wv-U37QL._SY606_.jpg'},
+        {name: 'Ravenclaw', members: 'Luna Lovegood, Cho Chang, Terry Boot, Michael Corner, Padma Patil, Penelope Clearwater, Marietta Edgecombe', logoUrl: 'https://images-na.ssl-images-amazon.com/images/I/91c9b3pZpXL._SY679_.jpg'},
+        {name: 'Hufflepuff', members: 'Susan Bones, Hannah Abbott, Cedric Diggory, Nymphadora Tonks, Justin Finch-Fletchley, Ernie Macmillan, Zacharias Smith', logoUrl: 'https://images-na.ssl-images-amazon.com/images/I/61gWZSTmkuL._AC_SL1000_.jpg'},
+        {name: 'Slytherin', members: 'Daphne Greengrass, Tracey Davis, Pansy Parkinson, Draco Malfoy, Vincent Crabbe, Gregory Goyle, Theodore Nott, Blaise Zabini', logoUrl: 'https://images-na.ssl-images-amazon.com/images/I/61ABoI-bAcL._AC_SL1100_.jpg'},
+      ]);
+    });
+};
