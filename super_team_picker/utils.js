2850b5eb0f2636f828c41911ccb8d9db84479617
Jiselle Liu <jiselle03@gmail.com>
2019-12-19 16:10:17 -0800

    Add comments for code

--- super_team_picker/utils.js
+++ super_team_picker/utils.js
@@ -1,4 +1,5 @@
 const utils = {
+    // Randomizing members
     shuffle(memberArr) {
         for (let i = 0; i < memberArr.length; i++) {
             let random = Math.floor(Math.random() * memberArr.length);
@@ -6,12 +7,15 @@ const utils = {
         }
         return memberArr;
     },
+    // Getting number of teams
     teamNums(shuffled, quantity) {
         return Math.ceil(shuffled.length / quantity);
     },
+    // Getting number of members per team
     numPer(shuffled, quantity) {
         return Math.floor(shuffled.length / quantity);
     },
+    // Finding the number of teams that have extra member
     extra(shuffled, quantity) {
         return shuffled.length % quantity;
     }
