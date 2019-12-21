dc3a383c0cc769de3c493a3ad904c1bcff134e89
Jiselle Liu <jiselle03@gmail.com>
2019-12-20 17:32:18 -0800

    Move more methods to utils.js to clean up views

--- super_team_picker/utils.js
+++ super_team_picker/utils.js
@@ -7,17 +7,43 @@ const utils = {
         }
         return memberArr;
     },
-    // Getting number of teams
-    teamNums(shuffled, quantity) {
-        return Math.ceil(shuffled.length / quantity);
+    // Number of Teams for perTeam
+    numTeams(memberArr, quantity) {
+        return Math.ceil(memberArr.length / quantity);
     },
-    // Getting number of members per team
-    numPer(shuffled, quantity) {
-        return Math.floor(shuffled.length / quantity);
+    // Assign teams using Per Team Method
+    perTeam(memberArr, quantity) {
+        let shuffled = this.shuffle(memberArr);
+        let numTeams = this.numTeams(memberArr, quantity);
+        let team = [];
+
+        for (let i = 1; i <= numTeams; i++) {
+            if (i !== numTeams) {
+                team.push(shuffled.slice(0, quantity));
+                shuffled.splice(0, quantity);
+            } else {
+                team.push(shuffled);
+            }
+        };
+        console.log(team)
+        return team;
     },
-    // Finding the number of teams that have extra member
-    extra(shuffled, quantity) {
-        return shuffled.length % quantity;
+    // Assign teams using Team Count Method
+    teamCount(memberArr, quantity) {
+        let shuffled = this.shuffle(memberArr);
+        let numPer = Math.floor(shuffled.length / quantity);
+        let extra = shuffled.length % quantity;
+        let team = [];
+
+        for (let j = 1; j <= extra; j++) {
+            team.push(shuffled.slice(0, numPer + 1));
+            shuffled.splice(0, numPer + 1);
+        }
+        for (let k = extra + 1; k <= quantity; k++) {
+            team.push(shuffled.slice(0, numPer));
+            shuffled.splice(0, numPer);
+        } 
+        return team;
     }
 };
 
