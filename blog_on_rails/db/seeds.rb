b2ea15678832ccf8975106751ee87727c282a42d
Jiselle Liu <jiselle03@gmail.com>
2020-01-11 23:17:38 -0800

    Update seeds file and reset database

--- blog_on_rails/db/seeds.rb
+++ blog_on_rails/db/seeds.rb
@@ -8,19 +8,47 @@
 
 Post.destroy_all()
 Comment.destroy_all()
+User.destroy_all()
 
-100.times do
+PASSWORD = "supersecret"  
+
+super_user = User.create( 
+    name: "Arya Stark", 
+    email: "noone@winterfell.gov", 
+    password: PASSWORD,
+    is_admin: true
+) 
+
+25.times do 
+    name = Faker::Movies::HarryPotter.character
+    email = "#{name.downcase.delete(" ")}@hogwarts.edu"
+    User.create( 
+        name: name,  
+        email: email, 
+        password: PASSWORD
+    )
+end 
+
+users = User.all 
+puts Cowsay.say("Created #{users.count} users", :tux)  
+puts "Login with #{super_user.email} and password of '#{PASSWORD}'"
+
+250.times do
+    user = users.sample 
     p = Post.create(
         title: Faker::TvShows::Buffy.episode,
         body: Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4),
         created_at: Faker::Date.backward(days:365 * 5),
-        updated_at: Faker::Date.backward(days:365 * 5)
+        updated_at: Faker::Date.backward(days:365 * 5),
+        user_id: user.id
     )
     if p.valid?
         p.comments = rand(0..10).times.map do
+            user = users.sample 
             Comment.new(
                 body: Faker::TvShows::Buffy.quote, 
                 created_at: Faker::Date.backward(days:365 * 5),
+                user_id: user.id
             )
         end
     end
