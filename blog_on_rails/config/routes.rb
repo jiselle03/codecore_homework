c14fed44e092018b43b002093def549f8c46beca
Jiselle Liu <jiselle03@gmail.com>
2020-01-11 23:19:48 -0800

    Configure routes to include users and session

--- blog_on_rails/config/routes.rb
+++ blog_on_rails/config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get('/', { to: 'welcome#index', as: 'root' })
+  get('/user/:id/password/edit', { to: 'users#edit_password', as: 'edit_password' })
   
   resources :posts do
     resources :comments, shallow: true, only: [:create, :destroy]
   end
+
+  resources :users, only: [:new, :create, :edit, :update]
+  resource :session, only: [:new, :create, :destroy]
 
 end
