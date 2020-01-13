0b93c9297917aa3d21462b2487ef71f294987749
Jiselle Liu <jiselle03@gmail.com>
2020-01-13 10:36:56 -0800

    Add authorization in comments controller

--- blog_on_rails/app/controllers/comments_controller.rb
+++ blog_on_rails/app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
 
     before_action :authenticate_user!
+    before_action :find_comment, only: [:destroy]
+    before_action :authorize!, only: [:destroy]
 
     def create
         @post = Post.find params[:post_id]
@@ -18,7 +20,6 @@ class CommentsController < ApplicationController
     end
 
     def destroy
-        @comment = Comment.find params[:id]
         @comment.destroy
         redirect_to post_path(@comment.post_id)
     end
@@ -27,6 +28,16 @@ class CommentsController < ApplicationController
 
     def comment_params
         params.require(:comment).permit(:body)
+    end
+
+    def find_comment
+        @comment = Comment.find params[:id]
+    end
+
+    def authorize!
+        unless can?(:crud, @comment)
+            redirect_to post_path(@comment.post_id), alert: 'Not Authorized'
+        end
     end
 
 end
