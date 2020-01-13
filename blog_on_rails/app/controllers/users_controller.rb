01240aace7b55cbb6e87ee0357c5ee9a4ce49f21
Jiselle Liu <jiselle03@gmail.com>
2020-01-13 13:21:59 -0800

    Clean up unnecessary code in users controller

--- blog_on_rails/app/controllers/users_controller.rb
+++ blog_on_rails/app/controllers/users_controller.rb
@@ -29,16 +29,6 @@ class UsersController < ApplicationController
         end
     end
 
-    def password
-        @password ||= Password.new(password_hash)
-    end
-    
-    def password=(new_password)
-        @password = Password.create(new_password)
-        self.password_hash = @password
-    end
-
-
     def edit_password
     end
 
